87d3ca768857c022858bd3a44ddb2ba4b98b8af4
Ashish Patel <ashish.tripleclicks@gmail.com>
2019-01-02 13:19:33 +0530

    add event listens to client side chat app

--- public/chat.js
+++ public/chat.js
@@ -1,17 +1,23 @@
 // make connection
 
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect("http://localhost:3000");
 
 // query dom
-var message = document.getElementById('msg');
-var handle = document.getElementById('handle');
-var sendBtn = document.getElementById('send');
-var output = document.getElementById('output');
+var message = document.getElementById("msg");
+var handle = document.getElementById("handle");
+var sendBtn = document.getElementById("send");
+var output = document.getElementById("output");
 
 // emit events
-sendBtn.addEventListener('click', () => {
-    socket.emit('chat-msg', {
-        message: message.value,
-        handle: handle.value
-    });
-})
+sendBtn.addEventListener("click", () => {
+  socket.emit("chat-msg", {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+// listen events
+socket.on("chat-msg", data => {
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
