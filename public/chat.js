91e865cce15cc4b6dc3f388b6c9c3d2b860c5071
Ashish Patel <ashish.tripleclicks@gmail.com>
2019-01-02 13:13:15 +0530

    added client side socket connection and event emitters

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,17 @@
 // make connection
 
 var socket = io.connect('http://localhost:3000');
+
+// query dom
+var message = document.getElementById('msg');
+var handle = document.getElementById('handle');
+var sendBtn = document.getElementById('send');
+var output = document.getElementById('output');
+
+// emit events
+sendBtn.addEventListener('click', () => {
+    socket.emit('chat-msg', {
+        message: message.value,
+        handle: handle.value
+    });
+})
